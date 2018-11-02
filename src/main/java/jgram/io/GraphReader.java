d2f23380f816c7231a8d1435127e2b831c72a1b9
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-11-03 01:49:58 +0330

    Adds skeleton of I/O utility classes: GraphReader + GraphWriter

---
+++ src/main/java/jgram/io/GraphReader.java
@@ -0,0 +1,21 @@
+/*** In The Name of Allah ***/
+package jgram.io;
+
+import org.jgrapht.Graph;
+
+/**
+ * Reads graph objects from various input sources.
+ * 
+ * @author Seyed Mohammad Ghaffarian
+ */
+public class GraphReader {
+    
+    public static Graph readDOT(String filePath) {
+        
+    }
+    
+    public static Graph readJSON(String filePath) {
+        throw new UnsupportedOperationException("Reading Graphs from JSON is NOT Implemented Yet!");
+    }
+    
+}
---
+++ src/main/java/jgram/io/GraphWriter.java
@@ -0,0 +1,56 @@
+/*** In The Name of Allah ***/
+package jgram.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import jgram.utils.StringUtils;
+import org.jgrapht.Graph;
+
+/**
+ * Writes graph objects to various output sources.
+ * 
+ * @author Seyed Mohammad Ghaffarian
+ */
+public class GraphWriter {
+    
+    public static void writeDOT(Graph graph, String filePath) throws IOException {
+        if (!filePath.toLowerCase().endsWith(".dot"))
+            throw new IllegalArgumentException("File-path does not end with .dot suffix!");
+		String filename = new File(filePath).getName();
+		try (PrintWriter dot = new PrintWriter(filePath, "UTF-8")) {
+            String graphName = filename.substring(0, filename.lastIndexOf('.'));
+            if (graph.getType().isDirected())
+                dot.println("digraph " + graphName + " {\n");
+            else
+                dot.println("graph " + graphName + " {\n");
+			Map<Object, String> nodeNames = new LinkedHashMap<>();
+			int nodeCounter = 1;
+			for (Object node: graph.vertexSet()) {
+				String name = "n" + nodeCounter++;
+				nodeNames.put(node, name);
+				StringBuilder label = new StringBuilder("   [label=\"");
+				if (!node.toString().isEmpty())
+    				label.append(StringUtils.escape(node.toString())).append("\"];");
+				dot.println("   " + name + label.toString());
+			}
+			dot.println();
+			for (Object edge: graph.edgeSet()) {
+				String src = nodeNames.get(graph.getEdgeSource(edge));
+				String trg = nodeNames.get(graph.getEdgeTarget(edge));
+				if (edge.toString().isEmpty())
+					dot.println("   " + src + " -> " + trg + ";");
+				else
+					dot.println("   " + src + " -> " + trg + "   [label=\"" + StringUtils.escape(edge.toString()) + "\"];");
+			}
+			dot.println("\n}");
+		}
+    }
+    
+    public static void writeJSON(Graph graph, String filePath) {
+        throw new UnsupportedOperationException("Writing Graphs to JSON is NOT Implemented Yet!");
+    }
+    
+}
