a8baf864f41e5466a8f9ecb499930ba6ab80d394
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-11-03 01:44:30 +0330

    Adds comment to sort method

--- src/main/java/jgram/utils/CollectionUtils.java
+++ src/main/java/jgram/utils/CollectionUtils.java
@@ -21,14 +21,15 @@ public class CollectionUtils {
      * @return     a new Map object with all entries sorted by value
      */
     public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        // Put map entries into a list
         List<Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        // Sort list based on value
         list.sort(Entry.comparingByValue());
-
+        // Put sorted list in an ordered-map
         Map<K, V> result = new LinkedHashMap<>();
-        for (Entry<K, V> entry : list) {
+        for (Entry<K, V> entry : list)
             result.put(entry.getKey(), entry.getValue());
-        }
-
+        // return sorted map
         return result;
     }
     
