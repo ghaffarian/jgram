78a72275aed53b65d846e5d62cb60d2bc4fbf665
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-11-03 01:43:44 +0330

    Removes init(...) + Changes Set to List

--- src/main/java/jgram/mining/frequent/FrequentSubgraphMining.java
+++ src/main/java/jgram/mining/frequent/FrequentSubgraphMining.java
@@ -1,7 +1,7 @@
 /*** In The Name of Allah ***/
 package jgram.mining.frequent;
 
-import java.util.Set;
+import java.util.List;
 import org.jgrapht.Graph;
 
 /**
@@ -12,16 +12,6 @@ import org.jgrapht.Graph;
 public interface FrequentSubgraphMining {
     
     /**
-     * Initialization of the algorithm.
-     * This method is optional.
-     * 
-     * @param params  any parameters required for initializing the algorithm
-     */
-    default void init(Object... params) {
-        ; // NOP!
-    }
-    
-    /**
      * Perform the mining for frequent subgraphs in the given graph set.
      * The result of this operation is a set of graphs 
      * which are frequent subgraphs in the input given graph set.
@@ -29,7 +19,7 @@ public interface FrequentSubgraphMining {
      * @param graphSet  the input set of graphs to be mined
      * @return          the result set of frequent subgraphs
      */
-    public Set<Graph> mine(Set<Graph> graphSet);
+    public List<Graph> mine(List<Graph> graphSet);
     
     /**
      * Fetch the set of frequent subgraphs resulted after the mining operation.
@@ -37,6 +27,6 @@ public interface FrequentSubgraphMining {
      * @return  returns the result of the mine(...) method (without running it again);
      *          or null if the mine(...) method has not completed
      */
-    public Set<Graph> getResult();
+    public List<Graph> getResult();
     
 }
