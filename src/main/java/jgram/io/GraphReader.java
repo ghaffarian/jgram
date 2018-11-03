2cf77a269b95e13cb9d5aa33ee94637249064f92
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-11-03 19:51:47 +0330

    Starting readDOT(...)

--- src/main/java/jgram/io/GraphReader.java
+++ src/main/java/jgram/io/GraphReader.java
@@ -1,6 +1,10 @@
 /*** In The Name of Allah ***/
 package jgram.io;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import org.jgrapht.Graph;
 
 /**
@@ -10,8 +14,42 @@ import org.jgrapht.Graph;
  */
 public class GraphReader {
     
-    public static Graph readDOT(String filePath) {
-        
+    public static Graph readDOT(String filePath) throws IOException {
+        if (!filePath.toLowerCase().endsWith(".dot"))
+            throw new IllegalArgumentException("File-path does not end with .dot suffix!");
+		String filename = new File(filePath).getName();
+		try (BufferedReader dot = new BufferedReader(new FileReader(filePath))) {
+            while (dot.ready()) {
+                String line = dot.readLine();
+                
+            }
+            dot.
+            String graphName = filename.substring(0, filename.lastIndexOf('.'));
+            if (graph.getType().isDirected())
+                dot.println("digraph " + graphName + " {\n");
+            else
+                dot.println("graph " + graphName + " {\n");
+			Map<Object, String> nodeNames = new LinkedHashMap<>();
+			int nodeCounter = 1;
+			for (Object node: graph.vertexSet()) {
+				String name = "n" + nodeCounter++;
+				nodeNames.put(node, name);
+				StringBuilder label = new StringBuilder("   [label=\"");
+				if (!node.toString().isEmpty())
+    				label.append(StringUtils.escape(node.toString())).append("\"];");
+				dot.println("   " + name + label.toString());
+			}
+			dot.println();
+			for (Object edge: graph.edgeSet()) {
+				String src = nodeNames.get(graph.getEdgeSource(edge));
+				String trg = nodeNames.get(graph.getEdgeTarget(edge));
+				if (edge.toString().isEmpty())
+					dot.println("   " + src + " -> " + trg + ";");
+				else
+					dot.println("   " + src + " -> " + trg + "   [label=\"" + StringUtils.escape(edge.toString()) + "\"];");
+			}
+			dot.println("\n}");
+		}
     }
     
     public static Graph readJSON(String filePath) {
