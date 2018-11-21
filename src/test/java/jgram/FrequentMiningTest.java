c09981b3afa54f550cdd8122705681ead202c8b6
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-11-21 18:56:46 +0330

    Adapt to changes of NaiveFSM.java

--- src/test/java/jgram/FrequentMiningTest.java
+++ src/test/java/jgram/FrequentMiningTest.java
@@ -3,7 +3,10 @@ package jgram;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import jgram.graphs.Graph;
 import jgram.io.GraphReader;
 import jgram.io.GraphWriter;
@@ -48,12 +51,29 @@ public class FrequentMiningTest {
         List<Graph<String, String>> graphDataset = GraphReader.readDotDataset("graph-data/set-1/");
         FrequentSubgraphMining<String, String> fsm = new NaiveFSM<>(0.10f);
         System.out.println("FSM started ...");
-        List<Graph<String, String>> patterns = fsm.mine(graphDataset);
-        System.out.println("FSM finished;  # of patterns = " + patterns.size());
+        Set<Graph<String, String>> patterns = fsm.mine(graphDataset);
+        System.out.println("FSM finished;  # of maximal-patterns = " + patterns.size());
         File patternsDir = new File("graph-data/out/patterns-1/");
         patternsDir.mkdirs();
-        for (int cnt = 0; cnt < patterns.size(); ++cnt)
-            GraphWriter.writeDOT(patterns.get(cnt), String.format("graph-data/out/patterns-1/pattern_%d.dot", cnt + 1));
+        int cnt = 1;
+        for (Graph<String, String> pattern: patterns) {
+            GraphWriter.writeDOT(pattern, String.format("graph-data/out/patterns-1/pattern_%d.dot", cnt));
+            ++cnt;
+        }
+        //assertTrue(patterns.size() >= 4);
+        //
+        graphDataset = GraphReader.readDotDataset("graph-data/set-2/");
+        fsm = new NaiveFSM<>(0.10f);
+        System.out.println("FSM started ...");
+        patterns = fsm.mine(graphDataset);
+        System.out.println("FSM finished;  # of maximal-patterns = " + patterns.size());
+        patternsDir = new File("graph-data/out/patterns-2/");
+        patternsDir.mkdirs();
+        cnt = 1;
+        for (Graph<String, String> pattern: patterns) {
+            GraphWriter.writeDOT(pattern, String.format("graph-data/out/patterns-2/pattern_%d.dot", cnt));
+            ++cnt;
+        }
         assertTrue(patterns.size() >= 4);
     }
 
