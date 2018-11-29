ff88ced80ee14a0aaaae04db370e86b2750277fc
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2018-11-29 20:18:37 +0330

    Switch from JGraphT to our own Graph + Name refactoring + Add count methods.

--- src/main/java/jgram/matching/Matching.java
+++ src/main/java/jgram/matching/Matching.java
@@ -3,29 +3,30 @@ package jgram.matching;
 
 import java.util.HashMap;
 import java.util.Map;
+import jgram.graphs.Edge;
 
 /**
  * Matching of two graphs.
  * Objects of this class can hold the result of a graph matching operation.
  * This class can be used both for exact (isomorphism) or inexact graph-matchings.
  * For exact graph-matching, the matching score is either 1 (isomorphic) or 0 (non-isomorphic).
- * For inexact graph-matching, the matching score can be any arbitrary integer value,
+ * For inexact graph-matching, the matching score can be any arbitrary floating-point value,
  * dependent on the matching algorithm.
  * 
  * @author Seyed Mohammad Ghaffarian
  */
 public class Matching<V,E> {
     
-    /* package private */ int score;
+    /* package private */ float score;
     
-    /* package private */ Map<V,V> nodeCorrespondence;
+    /* package private */ Map<V,V> vertexCorrespondence;
     
-    /* package private */ Map<E,E> edgeCorrespondence;
+    /* package private */ Map<Edge<V,E>,Edge<V,E>> edgeCorrespondence;
     
     /**
      * Creates a new Matching object, 
      * with score set to zero, and 
-     * empty correspondence for nodes and edges.
+     * empty correspondence for vertices and edges.
      */
     public Matching() {
         this(0);
@@ -34,25 +35,25 @@ public class Matching<V,E> {
     /**
      * Creates a new Matching object, 
      * with the given score value, and 
-     * empty correspondence for nodes and edges.
+     * empty correspondence for vertices and edges.
      * 
      * @param score  the matching score
      */
-    public Matching(int score) {
-        this(score, new HashMap<V,V>(), new HashMap<E,E>());
+    public Matching(float score) {
+        this(score, new HashMap<V,V>(), new HashMap<Edge<V,E>,Edge<V,E>>());
     }
     
     /**
      * Creates a new Matching object, 
      * with the given score, and correspondence for nodes and edges.
      * 
-     * @param score         the matching score
-     * @param nodeMappings  the correspondence of nodes from one graph to nodes of the other graph
-     * @param edgeMappings  the correspondence of edges from one graph to edges of the other graph
+     * @param score           the matching score
+     * @param vertexMappings  the correspondence of nodes from one graph to nodes of the other graph
+     * @param edgeMappings    the correspondence of edges from one graph to edges of the other graph
      */
-    public Matching(int score, Map<V,V> nodeMappings, Map<E,E> edgeMappings) {
+    public Matching(float score, Map<V,V> vertexMappings, Map<Edge<V,E>,Edge<V,E>> edgeMappings) {
         this.score = score;
-        nodeCorrespondence = nodeMappings;
+        vertexCorrespondence = vertexMappings;
         edgeCorrespondence = edgeMappings;
     }
     
@@ -61,19 +62,33 @@ public class Matching<V,E> {
      * 
      * @return  the matching score as an integer value
      */
-    public int getScore() {
+    public float getScore() {
        return score; 
+    }
+    
+    /**
+     * Get the number of vertex mappings in this matching instance.
+     */
+    public int getVertexCorrespondenceCount() {
+        return vertexCorrespondence.size();
+    }
+    
+    /**
+     * Get the number of edge mappings in this matching instance.
+     */
+    public int getEdgeCorrespondenceCount() {
+        return edgeCorrespondence.size();
     }
     
     /**
      * Get the corresponding node for the given node, based on this matching.
      * 
-     * @param node  the node to find its correspondence
-     * @return      the corresponding node for the given node, 
-     *              or null if there is no correspondence
+     * @param vertex  the node to find its correspondence
+     * @return        the corresponding node for the given node, 
+     *                or null if there is no correspondence
      */
-    public V getCorrespondingNode(V node) {
-        return nodeCorrespondence.get(node);
+    public V getCorrespondingVertex(V vertex) {
+        return vertexCorrespondence.get(vertex);
     }
     
     /**
@@ -83,7 +98,7 @@ public class Matching<V,E> {
      * @return      the corresponding edge for the given edge
      *              or null if there is no correspondence
      */
-    public E getCorrespondingEdge(E edge) {
+    public Edge<V,E> getCorrespondingEdge(Edge<V,E> edge) {
         return edgeCorrespondence.get(edge);
     }
     
