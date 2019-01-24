71d09eabe37bc2a857ecb8c8f462283c1da3f437
Seyed Mohammad Ghaffarian <s.m.ghaffarian@gmail.com>
2019-01-24 18:50:12 +0330

    Change FSM from Interface to Abstract Class

--- src/main/java/ghaffarian/jgram/mining/frequent/FrequentSubgraphMining.java
+++ src/main/java/ghaffarian/jgram/mining/frequent/FrequentSubgraphMining.java
@@ -1,17 +1,50 @@
 /*** In The Name of Allah ***/
 package ghaffarian.jgram.mining.frequent;
 
+import ghaffarian.graphs.Edge;
+import ghaffarian.graphs.Graph;
+import ghaffarian.graphs.Matcher;
 import java.util.List;
 import java.util.Set;
-import ghaffarian.graphs.Graph;
 
 /**
  * General interface for frequent subgraph mining algorithms.
- * 
+ *
  * @author Seyed Mohammad Ghaffarian
  */
-public interface FrequentSubgraphMining<V,E> {
+public abstract class FrequentSubgraphMining<V, E> {
+
+    protected Matcher<V> vertexMatcher;
+    protected Matcher<Edge<V,E>> edgeMatcher;
     
+    /**
+     * Set the vertex-matcher object.
+     */
+    public void setVertexMatcher(Matcher<V> matcher) {
+        vertexMatcher = matcher;
+    }
+    
+    /**
+     * Get the vertex-matcher object.
+     */
+    public Matcher<V> getVertexMatcher() {
+        return vertexMatcher;
+    }
+    
+    /**
+     * Set the edge-matcher object.
+     */
+    public void setEdgeMatcher(Matcher<Edge<V,E>> matcher) {
+        edgeMatcher = matcher;
+    }
+    
+    /**
+     * Get the edge-matcher object.
+     */
+    public Matcher<Edge<V,E>> getEdgeMatcher() {
+        return edgeMatcher;
+    }
+
     /**
      * Perform the mining for frequent subgraphs in the given graph set.
      * The result of this operation is a set of graphs 
@@ -20,7 +53,7 @@ public interface FrequentSubgraphMining<V,E> {
      * @param graphSet  the input set of graphs to be mined
      * @return          the result set of frequent subgraphs
      */
-    public Set<Graph<V,E>> mine(List<Graph<V,E>> graphSet);
+    public abstract Set<Graph<V,E>> mine(List<Graph<V,E>> graphSet);
     
     /**
      * Fetch the set of frequent subgraphs resulted after the mining operation.
@@ -28,6 +61,6 @@ public interface FrequentSubgraphMining<V,E> {
      * @return  returns the result of the mine(...) method (without running it again);
      *          or null if the mine(...) method has not completed
      */
-    public Set<Graph<V,E>> getResult();
+    public abstract Set<Graph<V,E>> getResult();
     
 }
